a94556348b9db80e4857c01c7c5b68c9b6ce732d
Steven Makunzva <stevenm@ZCTSTEMAK003.local>
2019-07-30 16:28:16 +0200

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,43 @@
+
+
+package factorial;
+
+public class Factorial {
+	
+	private static int num = 10;
+
+	public static void main(String[] args) {
+
+
+		// if(num == 0) {
+		// 	System.out.println(1);
+		// 	return;
+		// } else {
+
+		// 	int multiplier = 1;
+		// 	int factorial = 1;
+
+		// 	while(num != multiplier) {
+		// 		factorial = factorial * multiplier;
+		// 		multiplier ++;
+		// 	}
+
+		// 	System.out.println(num + " factorial = " + factorial);
+		// }
+
+		int result = recursiveFunction(num);
+
+		System.out.println("The factorial of: " + num + " = " + result);
+
+
+	}
+
+
+	private static int recursiveFunction(int num) {
+		if(num == 0) {
+			return 1;
+		}
+
+		return num * recursiveFunction(num -1);
+	}
+}
